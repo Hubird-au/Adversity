dddae1b4fdabe5b72d4bd51c5e6fe0860b0b12bc
Hubird <Hubird.au@gmail.com>
2014-05-23 09:40:22 +1000

    Still testing

--- Go.cmd
+++ Go.cmd
@@ -5,10 +5,16 @@ SortFilters.py Adversity.txt
 SortFilters.py Antisocial.txt
 SortFilters.py Extreme-Measures.txt
 SortFilters.py Adversity-Combined.txt
+@echo.
+echo Finished sorting...
+@echo.
 perl addChecksum.pl Adversity.txt
 perl addChecksum.pl Antisocial.txt
 perl addChecksum.pl Extreme-Measures.txt
 perl addChecksum.pl Adversity-Combined.txt
+@echo.
+echo Finished adding checksum
+@echo.
 cmd /k
 @echo.
 @echo.
