5e3203f04209777643c3f8844f8307284c413167
Hubird <Hubird.au@gmail.com>
2014-05-23 12:13:23 +1000

    http://www.rightthisminute.com/

--- Go.cmd
+++ Go.cmd
@@ -1,19 +1,21 @@
 @echo.
 @echo.
 join.py Adversity.txt Antisocial.txt Extreme-Measures.txt Adversity-Combined.txt
+@echo.
+@echo.
 SortFilters.py Adversity.txt
 SortFilters.py Antisocial.txt
 SortFilters.py Extreme-Measures.txt
 SortFilters.py Adversity-Combined.txt
 @echo.
-echo Finished sorting...
+@echo Finished sorting...
 @echo.
 perl addChecksum.pl Adversity.txt
 perl addChecksum.pl Antisocial.txt
 perl addChecksum.pl Extreme-Measures.txt
 perl addChecksum.pl Adversity-Combined.txt
 @echo.
-echo Finished adding checksum
+@echo Finished adding checksum
 @echo.
 cmd /k
 @echo.
