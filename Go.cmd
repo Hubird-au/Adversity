68a5ede8ea2425a064aa6d01948cda842fb21a82
Hubird <Hubird.au@gmail.com>
2014-05-23 14:58:57 +1000

    change go.cmd

--- Go.cmd
+++ Go.cmd
@@ -1,22 +1,8 @@
-@echo.
-@echo.
-join.py Adversity.txt Antisocial.txt Extreme-Measures.txt Adversity-Combined.txt
-@echo.
-@echo.
 SortFilters.py Adversity.txt
 SortFilters.py Antisocial.txt
 SortFilters.py Extreme-Measures.txt
 SortFilters.py Adversity-Combined.txt
-@echo.
-@echo Finished sorting...
 addChecksum.py Adversity.txt
 addChecksum.py Antisocial.txt
 addChecksum.py Extreme-Measures.txt
 addChecksum.py Adversity-Combined.txt
-@echo.
-@echo Finished adding checksum
-@echo.
-cmd /k
-@echo.
-@echo.
-
